abeb0eb6a5a618b4fde1748c50ef4237c7ee1cb0
Swathi <swathickswathi58@gmail.com>
2025-04-05 07:53:42 +0530

    refactor: update Dockerfile to use Node base image and streamline installation; enhance server setup in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import { allroutes } from "./routes/routes.js";
+import { allroutes } from "./routes/routes";
 import { serve } from "@hono/node-server";
 
-serve(allroutes, (info) => {
-  console.log(`Server is running on port ${info.port}`);
-});
+const port = 3000;
+
+serve(
+  {
+    fetch: allroutes.fetch,
+    port,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
